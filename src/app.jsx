57c8939b0661addd67d80872758fe95440d41da1
dulkifal <dulkifilibacker@gmail.com>
2023-01-01 12:36:42 +0530

    dowmload Name

--- src/app.jsx
+++ src/app.jsx
@@ -144,7 +144,7 @@ export function App(props) {
         <div className="Actions">
           {GeneratedData ? (
             <div>
-              <a href={GeneratedData} download="DHIU-Convocation">
+              <a href={GeneratedData} download="Mandharam">
                 <button>
                   <AiOutlineDownload size="30" />
                   <span>Download Profile</span>
